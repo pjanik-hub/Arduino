43cc48ed2df57bc97ebd4c8b21aea921eebfc7d8
Philip <philipjanik3@gmail.com>
2024-05-12 18:05:44 -0600

    formatting

--- led/led.ino
+++ led/led.ino
@@ -7,16 +7,17 @@
 /// May 12, 2024
 ///////////////////////////////////////
 
-#define NUM_LEDS     150      // 30 LEDs / meter w/ 5m = 150 LEDs total
-#define DATA_PIN     6        // PWM is for pin 6
-#define CHIPSET      WS2812B  // internal chipset (inside LED enclosure
-#define COLOR_ORDER  GRB      // WS2812B has a color order of GRB idk why 
-#define BRIGHTNESS   15       // 0x00 to 0xFF
+#define NUM_LEDS 150    // 30 LEDs / meter w/ 5m = 150 LEDs total
+#define DATA_PIN 6      // PWM is for pin 6
+#define CHIPSET WS2812B // internal chipset (inside LED enclosure
+#define COLOR_ORDER GRB // WS2812B has a color order of GRB idk why
+#define BRIGHTNESS 15   // 0x00 to 0xFF
 
 // the array of colors representing each LED
 CRGB leds[NUM_LEDS];
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   FastLED.addLeds<CHIPSET, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
 
@@ -25,13 +26,17 @@ void setup() {
   FastLED.show();
 }
 
-void loop() {
+void loop()
+{
+  static unsigned char r;
+
   // put your main code here, to run repeatedly:
   FastLED.clear();
-  
-  for (int i = 0; i < NUM_LEDS; i++) {
+
+  for (int i = 0; i < NUM_LEDS; i++)
+  {
     leds[i] = CRGB(255, 0, 0);
-      
+
     delay(10);
     FastLED.show();
   }
